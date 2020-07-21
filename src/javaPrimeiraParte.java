8b6425baf974d51339c3f703abae492fb0227598
Gabriel <g.dessilva95@gmail.com>
2020-07-20 22:04:04 -0300

    primeiras aulas de java

---
+++ src/javaPrimeiraParte.java
@@ -0,0 +1,8 @@
+public class javaPrimeiraParte {
+    public static void main(String[] args) {
+       int idade = 25;
+       String nome = "Gabriel";
+        System.out.println(nome+" tem "+idade+" anos de idade.");
+    }
+}
+
